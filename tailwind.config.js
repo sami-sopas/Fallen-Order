66b1c22cf66ee32c6f7ca5f86f5b2ea31e79ce28
sami-sopas <ramirezcuenca@outlook.es>
2023-08-15 17:14:33 -0600

    First configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: [
+        './public/**/*.html', //aplica el procesamiento post css
+    ],
+    darkMode : false, //or 'media' or 'class'
+    theme: {
+        extend: {}
+    },
+    variants : {
+        extend: {},
+    },
+    plugins: []
+}
